7d12d0bac781896d4302353aad001a461c51d056
christophpickl <christoph.pickl@gmail.com>
2017-02-28 13:09:42 +0100

    kdoc+dokka=javadoc work in progress

--- common/src/main/kotlin/com/github/christophpickl/kpotpourri/common/collection.kt
+++ common/src/main/kotlin/com/github/christophpickl/kpotpourri/common/collection.kt
@@ -1,8 +1,16 @@
 package com.github.christophpickl.kpotpourri.common
 
 
+/**
+ * List pretty print.
+ */
 fun List<Any>.toPrettyString() = map { "- ${it.toString()}" }.joinToString("\n")
 
+/**
+ * Does some pretty printing.
+ *
+ * @see toPrettyString
+ */
 fun List<Any>.prettyPrint() {
     println(toPrettyString())
 }
