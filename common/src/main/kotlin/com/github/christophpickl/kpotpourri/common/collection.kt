4bf563572cd51a2ce5147396a0f6b815484b293e
Christoph Pickl <c.pickl@meo-energy.com>
2017-03-31 20:12:24 +0200

    #44 release generator nearly done (filesystem stuff missing)

--- common/src/main/kotlin/com/github/christophpickl/kpotpourri/common/collection.kt
+++ common/src/main/kotlin/com/github/christophpickl/kpotpourri/common/collection.kt
@@ -4,7 +4,7 @@ package com.github.christophpickl.kpotpourri.common
 /**
  * List pretty print.
  */
-fun List<Any>.toPrettyString() = map { "- ${it.toString()}" }.joinToString("\n")
+fun List<Any>.toPrettyString(prefix: String = "- ") = map { listItem -> "$prefix$listItem" }.joinToString("\n")
 
 /**
  * Does some pretty printing by calling `toPrettyString`.
