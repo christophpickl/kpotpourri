4c802fd6dbf3843671e16b34fbc089f99e9b7e80
Christoph Pickl <c.pickl@meo-energy.com>
2017-04-05 20:29:29 +0200

    more tests

--- common/src/main/kotlin/com/github/christophpickl/kpotpourri/common/file/file.kt
+++ common/src/main/kotlin/com/github/christophpickl/kpotpourri/common/file/file.kt
@@ -1,14 +1,22 @@
 package com.github.christophpickl.kpotpourri.common.file
 
+import com.github.christophpickl.kpotpourri.common.KPotpourriException
 import java.io.File
 import java.nio.file.Files
 
-fun File.ensureExists() {
+
+/**
+ * Throws an exception if that file does not exist.
+ */
+fun File.verifyExists() { // formerly ensureExists()
     if (!exists()) {
-        throw RuntimeException("Expected a file existing at: $absolutePath")
+        throw KPotpourriException("Expected a file existing at: $absolutePath")
     }
 }
 
+/**
+ * Move a file by using JDK7's Files class.
+ */
 fun File.move(target: File) {
     Files.move(this.toPath(), target.toPath())
 }
--- common/src/test/kotlin/com/github/christophpickl/kpotpourri/common/file/file_test.kt
+++ common/src/test/kotlin/com/github/christophpickl/kpotpourri/common/file/file_test.kt
@@ -1,9 +1,24 @@
 package com.github.christophpickl.kpotpourri.common.file
 
+import com.github.christophpickl.kpotpourri.common.KPotpourriException
+import com.github.christophpickl.kpotpourri.common.testinfra.assertThrown
+import com.github.christophpickl.kpotpourri.common.testinfra.skip
 import org.testng.annotations.Test
+import java.io.File
 
 @Test class FileExtensionsTest {
 
-    // TODO implement me
+    fun `verifyExists - Given existing file, Should not throw`() {
+        File(".").verifyExists()
+    }
 
+    fun `verifyExists - Given non-existing file, Should throw`() {
+        assertThrown<KPotpourriException> {
+            File("./this/is/not/existing").verifyExists()
+        }
+    }
+
+    fun `move`() {
+        skip("how to test File.move()?")
+    }
 }
