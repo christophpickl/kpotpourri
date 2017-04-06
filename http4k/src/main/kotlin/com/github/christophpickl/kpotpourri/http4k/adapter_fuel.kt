331bc64b2d077f25c1b521f8e0765a3c742db12e
Christoph Pickl <c.pickl@meo-energy.com>
2017-04-06 20:09:22 +0200

    rename shc4k to http4k

---
+++ http4k/src/main/kotlin/com/github/christophpickl/kpotpourri/http4k/adapter_fuel.kt
@@ -0,0 +1,8 @@
+package com.github.christophpickl.kpotpourri.http4k
+
+
+//internal class FuelRestClient : RestClient {
+//    override fun execute(request: Request): Response {
+//        return Response(1, "")
+//    }
+//}
---
+++ http4k/src/test/kotlin/com/github/christophpickl/kpotpourri/http4k/RestClientFactoryTest.kt
@@ -0,0 +1,10 @@
+package com.github.christophpickl.kpotpourri.http4k
+
+import org.testng.annotations.Test
+
+@Test class RestClientFactoryTest {
+
+    fun `foobar`() {
+        println(RestClientFactory.get())
+    }
+}
