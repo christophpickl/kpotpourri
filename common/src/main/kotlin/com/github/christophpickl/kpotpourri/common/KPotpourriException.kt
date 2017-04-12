777f8bd4b8add8f054c8d0acb63a7f790b291415
Christoph Pickl <c.pickl@meo-energy.com>
2017-04-12 18:01:00 +0200

    rename projects to *4k

--- common/src/main/kotlin/com/github/christophpickl/kpotpourri/common/KPotpourriException.kt
+++
@@ -1,6 +0,0 @@
-package com.github.christophpickl.kpotpourri.common
-
-/**
- * Base class for any exception thrown from within this library.
- */
-open class KPotpourriException(message: String, cause: Exception? = null) : RuntimeException(message, cause)
--- common/src/main/kotlin/com/github/christophpickl/kpotpourri/common/numbers/numbers.kt
+++
@@ -1,7 +0,0 @@
-package com.github.christophpickl.kpotpourri.common.numbers
-
-
-/**
- * Format a double into a nice displayable representation.
- */
-fun Double.format(digits: Int) = java.lang.String.format("%.${digits}f", this)
