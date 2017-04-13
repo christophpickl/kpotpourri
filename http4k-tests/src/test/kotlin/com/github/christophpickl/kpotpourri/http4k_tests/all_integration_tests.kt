f7147f703d6697abcf2557f1ce392e26aede3ab7
christophpickl <christoph.pickl@gmail.com>
2017-04-13 15:50:06 +0200

    fix error

--- http4k-tests/src/test/kotlin/com/github/christophpickl/kpotpourri/http4k_tests/all_integration_tests.kt
+++ http4k-tests/src/test/kotlin/com/github/christophpickl/kpotpourri/http4k_tests/all_integration_tests.kt
@@ -1,18 +1,13 @@
 package com.github.christophpickl.kpotpourri.http4k_tests
 
 import com.github.christophpickl.kpotpourri.http4k.buildHttp4k
-import com.github.christophpickl.kpotpourri.http4k.integration_tests.AuthIT
-import com.github.christophpickl.kpotpourri.http4k.integration_tests.BaseUrlIT
-import com.github.christophpickl.kpotpourri.http4k.integration_tests.GetRequestsIT
-import com.github.christophpickl.kpotpourri.http4k.integration_tests.HeadersIT
-import com.github.christophpickl.kpotpourri.http4k.integration_tests.PostAndCoIT
-import com.github.christophpickl.kpotpourri.http4k.integration_tests.QueryParamsIT
-import com.github.christophpickl.kpotpourri.http4k.integration_tests.StatusCodeCheckIT
+import com.github.christophpickl.kpotpourri.http4k.integration_tests.*
+import com.github.christophpickl.kpotpourri.http4k.internal.MetaMap
 import com.github.christophpickl.kpotpourri.http4k_apache.ApacheHttpClientHttpImpl
 import com.github.christophpickl.kpotpourri.http4k_apache.apacheConnectTimeout
 import org.testng.annotations.Test
 
-val apacheProducer = { ApacheHttpClientHttpImpl(emptyMap()) }
+val apacheProducer = { ApacheHttpClientHttpImpl(MetaMap()) }
 
 @Test class AuthApacheIT : AuthIT(apacheProducer)
 @Test class BaseUrlApacheIT : BaseUrlIT(apacheProducer)
