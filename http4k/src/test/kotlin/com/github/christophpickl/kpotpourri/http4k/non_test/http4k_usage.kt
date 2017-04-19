cc3658fa86342a5e57b74b1c67b17b0717d405ec
christophpickl <christoph.pickl@gmail.com>
2017-04-19 23:39:57 +0200

    readme only

--- http4k/src/test/kotlin/com/github/christophpickl/kpotpourri/http4k/non_test/http4k_usage.kt
+++ http4k/src/test/kotlin/com/github/christophpickl/kpotpourri/http4k/non_test/http4k_usage.kt
@@ -1,5 +1,7 @@
 package com.github.christophpickl.kpotpourri.http4k.non_test
 
+import com.github.christophpickl.kpotpourri.http4k.SC_200_Ok
+import com.github.christophpickl.kpotpourri.http4k.buildHttp4k
 import com.github.christophpickl.kpotpourri.wiremock4k.WIREMOCK_PORT
 import com.github.tomakehurst.wiremock.WireMockServer
 import com.github.tomakehurst.wiremock.client.WireMock
@@ -16,10 +18,28 @@ fun main(args: Array<String>) {
 
         // execute HTTP4K ...
 
+        val http4k = buildHttp4k {
+            baseUrlBy("http://some.server/rest")
+            addHeader("someConstant" to "headerValue")
+        }
+        val dto = http4k.get("/resource", SomeDto::class) {
+            addHeader("Accept" to "my/content")
+            addQueryParam("sort" to "asc")
+        }
 
+        val response = http4k.get("/resource") {
+            addHeader("Accept" to "my/content")
+            addQueryParam("sort" to "asc")
+        }
+//        response.bodyAsString
+//        response.headers
+        if (response.statusCode != SC_200_Ok) {
+            // do something
+        }
     }
 }
 
+data class SomeDto (val string: String)
 
 
 fun withWiremock(port: Int = WIREMOCK_PORT, action: () -> Unit) {
