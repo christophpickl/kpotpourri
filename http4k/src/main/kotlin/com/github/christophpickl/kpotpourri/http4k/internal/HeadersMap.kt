43a6755a24192bedf24eb59698e373574f31eb56
christophpickl <christoph.pickl@gmail.com>
2017-05-03 19:28:26 +0200

    refactor headers map

--- http4k/src/main/kotlin/com/github/christophpickl/kpotpourri/http4k/internal/HeadersMap.kt
+++ http4k/src/main/kotlin/com/github/christophpickl/kpotpourri/http4k/internal/HeadersMap.kt
@@ -1,48 +1,19 @@
 package com.github.christophpickl.kpotpourri.http4k.internal
 
-import com.github.christophpickl.kpotpourri.common.logging.LOG
+import com.github.christophpickl.kpotpourri.common.collection.KeyIgnoringCaseMap
+import com.github.christophpickl.kpotpourri.common.collection.MapAddable
+import com.google.common.annotations.VisibleForTesting
 
 /**
  * Abstraction of a regular [Map] supporting specifics for a HTTP header (e.g. case-insensitive keys).
  */
-class HeadersMap {
+class HeadersMap(
+        private val _map: KeyIgnoringCaseMap<String> = KeyIgnoringCaseMap<String>(
+                disableLoggingOfKeys = listOf("authorization")
+        )
+) : MapAddable<String, String> by _map {
 
-    private val log = LOG {}
-
-    private val _map = HashMap<String, String>()
-
-    internal val map: Map<String, String> = _map
-
-    /**
-     * Simplify usage like "map += 1 to 2".
-     */
-    operator fun plusAssign(pair: Pair<String, String>) {
-        addEntry(pair.first, pair.second)
-    }
-
-    /**
-     * Add all entries from another [HeadersMap].
-     */
-    fun addAll(add: HeadersMap) {
-        addAll(add._map)
-    }
-
-    /**
-     *
-     */
-    fun addAll(add: Map<String, String>) {
-        add.forEach(this::addEntry)
-    }
-
-    private fun addEntry(key: String, value: String) {
-        map.keys.firstOrNull { it.toLowerCase() == key.toLowerCase() }?.let {
-            log.debug { "Overriding header '$it'. Old value: '${hideAuthorization(it, map[it]!!)}' with new value: '${hideAuthorization(it, value)}'" }
-            _map -= it
-        }
-        // ATTENTION: dont print header value as it could contain Authorization secret
-        _map += key to value
-    }
-
-    private fun hideAuthorization(key: String, value: String) = if (key.equals("authorization", ignoreCase = true)) "xxxxx" else value
+    /** Increase readability for tests, but read-only only. */
+    @VisibleForTesting internal val map: Map<String, String> = _map.toImmutableMap()
 
 }
