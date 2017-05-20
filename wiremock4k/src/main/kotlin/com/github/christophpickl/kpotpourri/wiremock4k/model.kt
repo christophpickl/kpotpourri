2855581512c8dd5a934bab976995fdb7d994f547
christophpickl <christoph.pickl@gmail.com>
2017-05-20 22:04:55 +0200

    test wiremock methods

--- wiremock4k/src/main/kotlin/com/github/christophpickl/kpotpourri/wiremock4k/model.kt
+++ wiremock4k/src/main/kotlin/com/github/christophpickl/kpotpourri/wiremock4k/model.kt
@@ -2,6 +2,7 @@ package com.github.christophpickl.kpotpourri.wiremock4k
 
 import com.github.tomakehurst.wiremock.client.MappingBuilder
 import com.github.tomakehurst.wiremock.client.WireMock.*
+import com.github.tomakehurst.wiremock.http.RequestMethod
 import com.github.tomakehurst.wiremock.matching.RequestPatternBuilder
 
 /**
@@ -22,24 +23,24 @@ val DEFAULT_WIREMOCK4K_PORT = 9987
  * Adding possibility to use methods as an object, indirecting to available methods via function calls only.
  */
 @Suppress("KDocMissingDocumentation")
-enum class WiremockMethod {
-    GET() {
+enum class WiremockMethod(val requestMethod: RequestMethod) {
+    GET(RequestMethod.GET) {
         override fun stubForPath(path: String) = get(urlEqualTo(path))!!
         override fun requestedFor(path: String) = getRequestedFor(urlEqualTo(path))!!
     },
-    POST() {
+    POST(RequestMethod.POST) {
         override fun stubForPath(path: String) = post(urlEqualTo(path))!!
         override fun requestedFor(path: String) = postRequestedFor(urlEqualTo(path))!!
     },
-    PUT() {
+    PUT(RequestMethod.PUT) {
         override fun stubForPath(path: String) = put(urlEqualTo(path))!!
         override fun requestedFor(path: String) = putRequestedFor(urlEqualTo(path))!!
     },
-    DELETE() {
+    DELETE(RequestMethod.DELETE) {
         override fun stubForPath(path: String) = delete(urlEqualTo(path))!!
         override fun requestedFor(path: String) = deleteRequestedFor(urlEqualTo(path))!!
     },
-    PATCH() {
+    PATCH(RequestMethod.PATCH) {
         override fun stubForPath(path: String) = patch(urlEqualTo(path))!!
         override fun requestedFor(path: String) = patchRequestedFor(urlEqualTo(path))!!
     }
@@ -52,11 +53,3 @@ enum class WiremockMethod {
     abstract fun requestedFor(path: String): RequestPatternBuilder
 
 }
-
-/**
- * Definition of a mocked request providing the HTTP method "on the fly".
- */
-data class MockRequest(
-        val path: String,
-        val withRequest: RequestPatternBuilder.() -> Unit = {}
-)
