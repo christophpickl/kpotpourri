890b80df1634309cc1889d5dc7596984ad30521e
christophpickl <christoph.pickl@gmail.com>
2017-08-23 23:44:58 +0200

    misc

--- release4k/src/test/kotlin/com/github/christophpickl/kpotpourri/release4k/release4k_for_kpot_app.kt
+++ release4k/src/test/kotlin/com/github/christophpickl/kpotpourri/release4k/release4k_for_kpot_app.kt
@@ -4,7 +4,12 @@ import com.github.christophpickl.kpotpourri.common.io.Keyboard
 import com.github.christophpickl.kpotpourri.release4k.Version.VersionParts2.Companion.readVersion2FromStdin
 import java.io.File
 
-fun main(args: Array<String>) = release4k {
+private val relativeKpotPath = "../github2/kpotpourri" // navigate to proper checkout location :) or simply "."
+private val liveKpotFolder = File(relativeKpotPath)
+private val gitUrl = "https://github.com/christophpickl/kpotpourri.git"
+private val versionTxtFilename = "version.txt"
+
+fun main(args: Array<String>) = release4k(workingDirectory = liveKpotFolder) {
     println()
     println("""
  _                _                               _
@@ -19,14 +24,10 @@ fun main(args: Array<String>) = release4k {
         throw RuntimeException("Invalid CWD! Execute this main class from 'kpotpourri' root directory, instead of submodule 'release4k' ;)")
     }
 
-    val relativeKpotPath = "../github2/kpotpourri" // navigate to proper checkout location :)
-    val liveKpotFolder = File(relativeKpotPath)
-    val versionTxtFilename = "version.txt"
-    val gitUrl = "https://github.com/christophpickl/kpotpourri.git"
-
     // =================================================================================================================
 
-    val currentVersion = readVersionFromTxt("$relativeKpotPath/$versionTxtFilename").toVersion2()
+    val relativeVersionPath = "$relativeKpotPath/$versionTxtFilename"
+    val currentVersion = readVersionFromTxt(relativeVersionPath).toVersion2()
     val nextVersion = readVersion2FromStdin(prompt = "Enter next release version", defaultVersion = currentVersion.incrementMinor())
     val nextVersionString = nextVersion.niceString
     val syspropNextVersion = "-Dkpotpourri.version=$nextVersionString"
@@ -43,7 +44,7 @@ fun main(args: Array<String>) = release4k {
     printHeader("RELEASE NOTES")
     println("Base release directory: ${release4kDirectory.canonicalPath}")
     println("GitHub URL: $gitUrl")
-    println("Version file: ${File(versionTxtFilename).canonicalPath}")
+    println("Version file: ${File(relativeVersionPath).canonicalPath}")
     println("Versions: ${currentVersion.niceString} => $nextVersionString")
     println()
 
