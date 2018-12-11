bc0f5c7e6107bbe81c9196cfcfb7812241e8ee7f
christophpickl <christoph.pickl@gmail.com>
2018-12-11 12:07:54 +0100

    better test script

--- bin/test.sh
+++ bin/test.sh
@@ -3,13 +3,10 @@
 echo "./gradlew clean test"
 
 ./gradlew clean test
-
 RESULT_CODE=$?
-echo "[KPOT] tests DONE."
 
-# Samantha, Tessa, Victoria, Anna (DE)
-#if [ ${RESULT_CODE} -eq 0 ]; then
-#    say -v Anna "Die Tests wurden erfolgreich abgeschlossen."
-#else
-#    say -v Anna "Die Tests sind fehlgeschlagen."
-#fi
+if [[ ${RESULT_CODE} -eq 0 ]]; then
+    echo "[KPOT] tests SUCCESS."
+else
+    echo "[KPOT] tests FAILED."
+fi
